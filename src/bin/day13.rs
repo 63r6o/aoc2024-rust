270e2938f9ffd85af047bc7df078221ee907402c
Gergo <pancho.puskas@protonmail.com>
2024-12-16 23:35:09 +0100

    day 14

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -6,9 +6,9 @@ use regex::Regex;
 #[derive(Debug)]
 struct Position {
     used_tokens: i64,
-    coords : (i64, i64),
+    coords: (i64, i64),
     a_steps: i64,
-    b_steps: i64
+    b_steps: i64,
 }
 
 impl Eq for Position {}
@@ -54,7 +54,10 @@ fn main() {
         .collect();
 
     // my original solution for part 1
-    let dijstra_like_stuff = |(x_a, y_a): (i64, i64), (x_b, y_b): (i64, i64), (x_prize, y_prize): (i64, i64)| -> (i64, i64) {
+    let dijstra_like_stuff = |(x_a, y_a): (i64, i64),
+                              (x_b, y_b): (i64, i64),
+                              (x_prize, y_prize): (i64, i64)|
+     -> (i64, i64) {
         let mut seen: HashSet<(i64, i64)> = HashSet::new();
         let mut min_heap: BinaryHeap<Position> = BinaryHeap::new();
 
@@ -70,32 +73,45 @@ fn main() {
         while !min_heap.is_empty() {
             let current_position = min_heap.pop().unwrap();
 
-            if seen.contains(&current_position.coords) || current_position.a_steps > 100 || current_position.b_steps > 100 {
+            if seen.contains(&current_position.coords)
+                || current_position.a_steps > 100
+                || current_position.b_steps > 100
+            {
                 continue;
             }
 
             seen.insert(current_position.coords);
 
             if current_position.coords == (x_prize, y_prize) {
-                return (current_position.a_steps, current_position.b_steps)
+                return (current_position.a_steps, current_position.b_steps);
             }
 
             // 3 tokens for A 1 tokens for B
-            if current_position.coords.0 + x_a <= x_prize && current_position.coords.1 + y_a <= y_prize {
+            if current_position.coords.0 + x_a <= x_prize
+                && current_position.coords.1 + y_a <= y_prize
+            {
                 min_heap.push(Position {
                     used_tokens: current_position.used_tokens + 3,
-                    coords: (current_position.coords.0 + x_a, current_position.coords.1 + y_a),
+                    coords: (
+                        current_position.coords.0 + x_a,
+                        current_position.coords.1 + y_a,
+                    ),
                     a_steps: current_position.a_steps + 1,
-                    b_steps: current_position.b_steps
+                    b_steps: current_position.b_steps,
                 })
             }
 
-            if current_position.coords.0 + x_b <= x_prize && current_position.coords.1 + y_b <= y_prize {
+            if current_position.coords.0 + x_b <= x_prize
+                && current_position.coords.1 + y_b <= y_prize
+            {
                 min_heap.push(Position {
                     used_tokens: current_position.used_tokens + 1,
-                    coords: (current_position.coords.0 + x_b, current_position.coords.1 + y_b),
+                    coords: (
+                        current_position.coords.0 + x_b,
+                        current_position.coords.1 + y_b,
+                    ),
                     a_steps: current_position.a_steps,
-                    b_steps: current_position.b_steps + 1
+                    b_steps: current_position.b_steps + 1,
                 })
             }
         }
@@ -105,7 +121,10 @@ fn main() {
 
     // For part 2, I couldn't figure it out by myself
     // https://www.reddit.com/r/adventofcode/comments/1hd7irq/2024_day_13_an_explanation_of_the_mathematics/
-    let solve_it_with_maths = |(x_a, y_a): (i64, i64), (x_b, y_b): (i64, i64), (mut x_prize, mut y_prize): (i64, i64)| -> (i64, i64) {
+    let solve_it_with_maths = |(x_a, y_a): (i64, i64),
+                               (x_b, y_b): (i64, i64),
+                               (mut x_prize, mut y_prize): (i64, i64)|
+     -> (i64, i64) {
         x_prize = x_prize + 10000000000000;
         y_prize = y_prize + 10000000000000;
 
@@ -113,7 +132,7 @@ fn main() {
         let b = (x_a * y_prize - y_a * x_prize) / (x_a * y_b - y_a * x_b);
 
         if (a * x_a + b * x_b, a * y_a + b * y_b) == (x_prize, y_prize) {
-            return (a, b)
+            return (a, b);
         }
 
         (0, 0)
@@ -123,7 +142,11 @@ fn main() {
     let mut part_two = 0;
     claws.iter().for_each(|coords| {
         let (a_steps_one, b_steps_one) = dijstra_like_stuff(coords[0], coords[1], coords[2]);
-        let (a_steps_two, b_steps_two) = solve_it_with_maths(coords[0], coords[1], (coords[2].0 as i64, coords[2].1 as i64));
+        let (a_steps_two, b_steps_two) = solve_it_with_maths(
+            coords[0],
+            coords[1],
+            (coords[2].0 as i64, coords[2].1 as i64),
+        );
 
         part_one += (a_steps_one * 3) + b_steps_one;
         part_two += (a_steps_two * 3) + b_steps_two;
---
+++ src/bin/day14.rs
@@ -0,0 +1,116 @@
+use aoc2024_rust::read_from_args;
+
+fn main() {
+    let input = read_from_args().expect("Couldn't open the input file");
+
+    let robots: Vec<((i32, i32), (i32, i32))> = input
+        .lines()
+        .filter_map(|line| {
+            line.split_once(' ').map(|(pos, vel)| {
+                let positions = pos.split_once('=').map(|(_, p)| {
+                    p.split_once(',')
+                        .map(|(x, y)| (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()))
+                });
+
+                let velocity = vel.split_once('=').map(|(_, v)| {
+                    v.split_once(',')
+                        .map(|(x, y)| (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()))
+                });
+
+                (positions.flatten().unwrap(), velocity.flatten().unwrap())
+            })
+        })
+        .collect();
+
+    let width = 101;
+    let height = 103;
+    let steps = 100;
+
+    let part_one: i32 = robots
+        .iter()
+        .map(|(pos, vel)| {
+            let x_offset = (pos.0 + (vel.0 * steps)) % width;
+            let y_offset = (pos.1 + (vel.1 * steps)) % height;
+
+            let new_x = if x_offset < 0 {
+                width + x_offset
+            } else {
+                x_offset
+            };
+            let new_y = if y_offset < 0 {
+                height + y_offset
+            } else {
+                y_offset
+            };
+
+            (new_x, new_y)
+        })
+        .fold([0, 0, 0, 0], |mut quadrants, (x, y)| {
+            match (x.cmp(&(width / 2)), y.cmp(&(height / 2))) {
+                (std::cmp::Ordering::Greater, std::cmp::Ordering::Less) => quadrants[2] += 1,
+                (std::cmp::Ordering::Greater, std::cmp::Ordering::Greater) => quadrants[3] += 1,
+                (std::cmp::Ordering::Less, std::cmp::Ordering::Less) => quadrants[0] += 1,
+                (std::cmp::Ordering::Less, std::cmp::Ordering::Greater) => quadrants[1] += 1,
+                _ => (),
+            }
+
+            quadrants
+        })
+        .iter()
+        .product();
+
+    let mut part_two = 0;
+    loop {
+        let current_state: Vec<(i32, i32)> = robots
+            .iter()
+            .map(|(pos, vel)| {
+                let x_offset = (pos.0 + (vel.0 * part_two)) % width;
+                let y_offset = (pos.1 + (vel.1 * part_two)) % height;
+
+                let new_x = if x_offset < 0 {
+                    width + x_offset
+                } else {
+                    x_offset
+                };
+                let new_y = if y_offset < 0 {
+                    height + y_offset
+                } else {
+                    y_offset
+                };
+
+                (new_x, new_y)
+            })
+            .collect();
+
+        if current_state
+            .iter()
+            .filter(|pos| {
+                pos.0 > 1
+                    && pos.0 < width - 1
+                    && pos.1 < height - 1
+                    && current_state.contains(&(pos.0 - 1, pos.1 + 1))
+                    && current_state.contains(&(pos.0 + 1, pos.1 + 1))
+            })
+            .collect::<Vec<&(i32, i32)>>()
+            .len()
+            > current_state.len() / 2 / 3
+        {
+            let mut pic = [[' '; 101]; 103];
+            current_state.iter().for_each(|(x, y)| {
+                pic[*y as usize][*x as usize] = 'x';
+            });
+
+            for y in 0..height {
+                for x in 0..width {
+                    print!("{}", pic[y as usize][x as usize])
+                }
+                println!()
+            }
+            break;
+        }
+        part_two += 1;
+    }
+
+    println!("part one: {part_one}");
+    println!("part two: {part_two}");
+}
